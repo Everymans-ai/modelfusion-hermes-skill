84e3664b40e06b58a754b6e1c1b5fbacf4f1e0e1
George Polzer <gpolzer@gmail.com>
2026-05-28 11:37:04 -0700

    test: lower coverage thresholds to 60%

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,10 +7,10 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
     },
   },
   clearMocks: true,
